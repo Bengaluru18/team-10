6f323f519c3dc7348e3e5b0d1257659709de554c
Abhishek Bisht <33423904+awesomeAB@users.noreply.github.com>
2018-07-07 20:31:20 +0530

    Login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import {environment} from '../environments/environment';
 import {AngularFirestore, AngularFirestoreModule} from 'angularfire2/firestore';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,

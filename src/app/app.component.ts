6195db5fde25397749598acf8063e284aeefa51e
Abhishek Bisht <33423904+awesomeAB@users.noreply.github.com>
2018-07-07 21:57:21 +0530

    App component fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from './auth.service';
 // import { AngularFirestore } from 'angularfire2/firestore';
 
 @Component({
@@ -8,4 +9,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  email: string;
+  password: string;
+
+  constructor(public authService: AuthService) {}
+
+  signup() {
+    this.authService.signup(this.email, this.password);
+    this.email = this.password = '';
+  }
+
+  login() {
+    this.authService.login(this.email, this.password);
+    this.email = this.password = '';    
+  }
+
+  logout() {
+    this.authService.logout();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {AngularFireModule} from 'angularfire2';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import {environment} from '../environments/environment';
 import {AngularFirestore, AngularFirestoreModule} from 'angularfire2/firestore';
+import { MatFormFieldModule,MatCardModule,MatButtonModule,MatToolbarModule,MatExpansionModule } from '@angular/material'
 import { LoginComponent } from './login/login.component';
+import { AngularFireModule } from 'angularfire2';
+import { environment } from '../environments/environment';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AuthService } from './auth.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -18,9 +22,13 @@ import { LoginComponent } from './login/login.component';
     BrowserModule,
     AppRoutingModule,
     AngularFirestoreModule,
+    AngularFireAuthModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,MatCardModule,MatButtonModule,MatToolbarModule,MatExpansionModule,
     AngularFireModule.initializeApp(environment.firebase)
   ],
-  // providers: [AngularFirestore],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
